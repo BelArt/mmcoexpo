181aea90b2459b4e7c3386454fcc33c1bdc36101
Aleksandr Filippov <philippov_sander@mail.ru>
2021-05-12 13:36:53 +0300

    Изменяет credentials для google

--- resource/config/setting.php
+++ resource/config/setting.php
@@ -4,23 +4,24 @@
  */
 
 return [
-    'amo'    => [
-        'email'  => 'goncharov_vs@mail.ru',
-        'hash'   => 'dd3ecba7e47f6e87e96f357f26f08943cd14ff1b',
+    'amo' => [
+        'email' => 'goncharov_vs@mail.ru',
+        'hash' => 'dd3ecba7e47f6e87e96f357f26f08943cd14ff1b',
         'domain' => 'mmcoexpo',
     ],
     'google' => [
-        'client_id'                  => '419107525873-kb83s127jq9o1t38fhsqa28p2s7a4kbg.apps.googleusercontent.com',
-        'secret'                     => '06SKo-SOpidOfY49-dSiG92m',
-        'request_spreadsheet_id'     => '100_fjq5SF8vrXWtH4PRaVTT_65LFaA9I6aQC-dqY9UM',
+        'client_id' => '419107525873-kb83s127jq9o1t38fhsqa28p2s7a4kbg.apps.googleusercontent.com',
+        'secret' => '06SKo-SOpidOfY49-dSiG92m',
+        'request_spreadsheet_id' => '100_fjq5SF8vrXWtH4PRaVTT_65LFaA9I6aQC-dqY9UM',
         'four_meters_spreadsheet_id' => '1nsVLNETSxR7SCjgFvxmxchZac6jHnE2CunvwAxENFEA',
-        'documents_signed_report'    => '1RfhqDbThDoso7ZaLbjilPPSDanOL2cczg0cTkMc1I9I',
-        'access'                     => [
-            'access_token'  => 'ya29.GltkB08SFfqmY3HfWL2xwryLUUkQ8NgojXc30YX58L8bKnMK9ugABBmhHSXYJUpTH_2bei_vB1i7ZkdN806K9sNle4CP9bFiqqdKWrp2NOq023kNQc9PqNv-byYe',
-            'token_type'    => 'Bearer',
-            'expires_in'    => '3600',
-            'refresh_token' => '1/qkBqpwAmMl4HEU3sJ5TnaA10ySLHnUVKpwYK-qdDtSo',
-            'created'       => '1565796523',
+        'documents_signed_report' => '1RfhqDbThDoso7ZaLbjilPPSDanOL2cczg0cTkMc1I9I',
+        'access' => [
+            'access_token' => 'ya29.a0AfH6SMBDGnraiwbAp7-XvZ_ZG6rVY9TPh7zjBVUzBdsATvqSHojVVwQ27LN5XJ524eTtxw1-oX1n6c45jsObKrgAf321FXjSZ1B_Y_zH3Ht6TvsB47O1JmXa6ouggH3b9v5PhFot8hxOllp20fmuSOdgDYAs',
+            'token_type' => 'Bearer',
+            'expires_in' => '3599',
+            'scope' => 'https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive',
+            'refresh_token' => '1//0cSfmX14iCpsACgYIARAAGAwSNwF-L9IrsQCxvxO0h8xE5JbH3omFO-HciKqdO2ZgngQDAF2fWyme_AyfODuTjwkONHOzd03n5wM',
+            'created' => '1620815704',
         ],
     ],
 ];
